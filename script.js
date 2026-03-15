f0ef20c89391d0f76bf37633ba3300a2ca6a9444
LarbiG31 <ghalem-larbi@hotmail.fr>
2026-03-15 00:28:41 -0400

    V2
    Uplode new version

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function setLang(lang){
-
-document.querySelectorAll("[data-en]").forEach(el=>{
-
-el.innerText = el.dataset[lang]
-
-})
-
+function setLang(lang) {
+    document.querySelectorAll(`[data-${lang}]`).forEach(el => {
+        el.innerText = el.getAttribute(`data-${lang}`);
+    });
+    localStorage.setItem('preferredLang', lang);
 }
+
+window.onload = () => {
+    const savedLang = localStorage.getItem('preferredLang') || 'en';
+    setLang(savedLang);
+};
