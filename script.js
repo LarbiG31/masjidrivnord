1ae8b95d9905ebdb161ed2a05ee5e559f86dc044
LarbiG31 <ghalem-larbi@hotmail.fr>
2026-03-15 00:37:31 -0400

    V2.1
    New version
    Fix overlap issue

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function setLang(lang) {
     document.querySelectorAll(`[data-${lang}]`).forEach(el => {
         el.innerText = el.getAttribute(`data-${lang}`);
     });
-    localStorage.setItem('preferredLang', lang);
+    localStorage.setItem('prefLang', lang);
 }
-
 window.onload = () => {
-    const savedLang = localStorage.getItem('preferredLang') || 'en';
-    setLang(savedLang);
+    setLang(localStorage.getItem('prefLang') || 'en');
 };
+
